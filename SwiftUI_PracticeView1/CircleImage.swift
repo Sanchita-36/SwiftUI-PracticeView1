88b655a710a107794efc987ff7c70b7963298b77
Sanchita-36 <sanchitalachke@gmail.com>
2024-11-28 15:09:14 +0530

    Initial Commit

---
+++ SwiftUI_PracticeView1/CircleImage.swift
@@ -0,0 +1,18 @@
+//
+//  CircleImage.swift
+//  SwiftUI_PracticeView1
+//
+//  Created by sanchita lachke on 28/11/24.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
---
+++ SwiftUI_PracticeView1/MapView.swift
@@ -0,0 +1,18 @@
+//
+//  MapView.swift
+//  SwiftUI_PracticeView1
+//
+//  Created by sanchita lachke on 28/11/24.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapView()
+}
