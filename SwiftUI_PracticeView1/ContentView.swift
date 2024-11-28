1c915f154e2efa6a5255bf078d6c43bee1ed6163
Sanchita-36 <sanchitalachke@gmail.com>
2024-11-28 15:09:14 +0530

    Initial Commit

---
+++ SwiftUI_PracticeView1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI_PracticeView1
+//
+//  Created by sanchita lachke on 28/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI_PracticeView1/SwiftUI_PracticeView1App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_PracticeView1App.swift
+//  SwiftUI_PracticeView1
+//
+//  Created by sanchita lachke on 28/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_PracticeView1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
